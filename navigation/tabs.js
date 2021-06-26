05e9b120010dec1a7ae7172732489ca70da28508
esteloabell <manitobaaa@gmail.com>
2021-06-26 13:34:03 -0500

    added TabIcon component

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -4,28 +4,60 @@ import {
 } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
+import { TabIcon } from "../components";
 import { Home } from "../screens"
+import { COLORS, icons } from "../constants";
 
 const Tab = createBottomTabNavigator()
 
 const Tabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          backgroundColor: COLORS.white,
+          borderColor: "transparent",
+          height: 60
+        }
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => 
+          <TabIcon focused = {focused} icon={icons.home} />
+        }}
       />
       <Tab.Screen
         name="Search"
         component={Home}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            <TabIcon focused={focused} icon={icons.search} />
+        }}
       />
       <Tab.Screen
         name="Bookmark"
         component={Home}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            <TabIcon focused={focused} icon={icons.bookmark} />
+        }}
       />
       <Tab.Screen
         name="Settings"
         component={Home}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            <TabIcon focused={focused} icon={icons.settings} />
+        }}
       />
     </Tab.Navigator>
   )
